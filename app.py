3f9b2529b921ce39d0d82100ebdd51b5afae893a
Szymon Rogowski <szymon0r0official@gmail.com>
2023-11-29 19:35:36 +0100

    bunch of stuff

--- app.py
+++ app.py
@@ -1,15 +1,36 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
+import sqlite3
 
 app = Flask(__name__)
+DATABASE = 'database.db'
+
+
+@app.route('/save_data', methods=['POST'])
+def save_data():
+    data = request.get_json()
+
+    conn = sqlite3.connect(DATABASE)
+    cursor = conn.cursor()
+    cursor.execute('''
+        INSERT INTO suggestions (problem_desc, name, email, phone)
+        VALUES (?, ?, ?, ?)
+    ''', (data['problem_description'], data['name'], data['email'], data['phone']))
+
+    conn.commit()
+    conn.close()
+
+    return jsonify({'message': 'Data saved successfully'})
 
 
 @app.route('/')
 def main():
     return render_template('main.html')
 
+
 @app.route('/panel')
 def panel():
     return render_template('panel.html')
+
 
 @app.route('/panel/dij_a_aco')
 def dij_a_aco():
--- static/js/main.js
+++ static/js/main.js
@@ -1,9 +1,39 @@
-
-
 document.addEventListener('DOMContentLoaded', function () {
     var loadButton = document.querySelector('.big-button');
+    var submitButton = document.querySelector('#contact-form button[type="submit"]');
 
     loadButton.addEventListener('click', function () {
         window.location.href = '/panel';
+    });
+
+    submitButton.addEventListener('click', function (event) {
+        event.preventDefault();
+
+        var problemDescription = document.querySelector('textarea[name="problem_description"]').value;
+        var name = document.querySelector('input[name="name"]').value;
+        var email = document.querySelector('input[name="email"]').value;
+        var phoneNumber = document.querySelector('input[name="phone"]').value;
+
+        var formData = {
+            problem_description: problemDescription,
+            name: name,
+            email: email,
+            phone: phoneNumber
+        };
+
+        fetch('/save_data', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(formData),
+        })
+        .then(response => response.json())
+        .then(data => {
+            console.log('Success:', data);
+        })
+        .catch((error) => {
+            console.error('Error:', error);
+        });
     });
 });
