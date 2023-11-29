e3c5c50bd3f2c4f58ba0a53d0169495a3a6a794c
Szymon Rogowski <szymon0r0official@gmail.com>
2023-11-29 19:20:11 +0100

    bunch of stuff

--- app.py
+++ app.py
@@ -11,22 +11,22 @@ def main():
 def panel():
     return render_template('panel.html')
 
-# @app.route('/panel/dij_a_aco')
-# def dij_a_aco():
-#     print("USED dij_a_aco")
-#     return render_template('rrt_dij_a_aco.html')
-#
-#
-# @app.route('/panel/rrt')
-# def rrt():
-#     print("USED rrt")
-#     return render_template('rrt_dij_a_aco.html')
-#
-#
-# @app.route('/panel/dqn')
-# def dqn():
-#     print("USED dqn")
-#     return render_template('dqn.html')
+@app.route('/panel/dij_a_aco')
+def dij_a_aco():
+    print("USED dij_a_aco")
+    return render_template('rrt_dij_a_aco.html')
+
+
+@app.route('/panel/rrt')
+def rrt():
+    print("USED rrt")
+    return render_template('rrt_dij_a_aco.html')
+
+
+@app.route('/panel/dqn')
+def dqn():
+    print("USED dqn")
+    return render_template('dqn.html')
 
 
 if __name__ == '__main__':
---
+++ static/js/main.js
@@ -0,0 +1,9 @@
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    var loadButton = document.querySelector('.big-button');
+
+    loadButton.addEventListener('click', function () {
+        window.location.href = '/panel';
+    });
+});
