ee7d26da95f905ceb66dc06f5dde19ec1e975293
Szymon Rogowski <szymon0r0official@gmail.com>
2023-11-29 16:22:54 +0100

    bunch of stuff

--- app.py
+++ app.py
@@ -4,26 +4,29 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def index():
-    return render_template('index.html')
+def main():
+    return render_template('main.html')
 
+@app.route('/panel')
+def panel():
+    return render_template('panel.html')
 
-@app.route('/dij_a_aco')
-def dij_a_aco():
-    print("USED dij_a_aco")
-    return render_template('rrt_dij_a_aco.html')
-
-
-@app.route('/rrt')
-def rrt():
-    print("USED rrt")
-    return render_template('rrt_dij_a_aco.html')
-
-
-@app.route('/dqn')
-def dqn():
-    print("USED dqn")
-    return render_template('dqn.html')
+# @app.route('/panel/dij_a_aco')
+# def dij_a_aco():
+#     print("USED dij_a_aco")
+#     return render_template('rrt_dij_a_aco.html')
+#
+#
+# @app.route('/panel/rrt')
+# def rrt():
+#     print("USED rrt")
+#     return render_template('rrt_dij_a_aco.html')
+#
+#
+# @app.route('/panel/dqn')
+# def dqn():
+#     print("USED dqn")
+#     return render_template('dqn.html')
 
 
 if __name__ == '__main__':
